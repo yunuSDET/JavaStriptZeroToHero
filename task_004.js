b4ed693487ae21d19fd74de7cc36bdec41c342dd
yunuSdet <yunuskulcu84@gmail.com>
2023-11-22 22:57:06 +0300

    task_004.js is updated

--- task_004.js
+++ task_004.js
@@ -1,3 +1,3 @@
 //Check if given string variable starts with "a" and has less than five characters (output must be true or false)
 let str = "all";
-console.log(str.startsWith("a"));
+console.log(str.startsWith("a") && str.length<5);
