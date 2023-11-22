0c59f992eb9f6212ec0300a21c5556de1e1d1513
yunuSdet <yunuskulcu84@gmail.com>
2023-11-22 23:42:22 +0300

    task_006.js is updated

--- task_006.js
+++ task_006.js
@@ -1,2 +1,17 @@
 //Print the odd numbers from an integer array;
 let arr = [1,2,3,4,5,6,7,8,9,12,114,41];
+
+
+//First solution
+for (let each of arr) 
+    if(each % 2 == 1) console.log(each);
+
+
+
+//Second solution
+arr.filter(i =>i % 2 == 1).forEach(i=>console.log(i));
+
+
+
+//Third solution
+arr.forEach(i=> {if(i % 2 ==1) console.log(i)})
