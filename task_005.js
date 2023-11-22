758b5b21294df947cd50a6a228dc480270100c80
yunuSdet <yunuskulcu84@gmail.com>
2023-11-22 23:31:44 +0300

    task_006.js is added

--- task_005.js
+++ task_005.js
@@ -4,9 +4,7 @@ let arr = [1,2,3,4,5,6,7,8,9];
 
 //First solution
 let sum1 = 0;
-for (let each of arr) {
-    sum1 +=each
-}
+for (let each of arr) sum1 +=each;
 console.log(sum1);
 
 
@@ -15,10 +13,10 @@ console.log(sum1);
 //Second solution
 let sum2 = 0;
 arr.forEach(each => sum2 += each);
-console.log(sum2)
+console.log(sum2);
 
 
 
 //Third solution
-let sum3 = arr.reduce((acc,currentValue) => acc + currentValue,0);
-console.log(sum3)
+let sum3 = arr.reduce((acc,each) => acc + each,0);
+console.log(sum3);
---
+++ task_006.js
@@ -0,0 +1,2 @@
+//Print the odd numbers from an integer array;
+let arr = [1,2,3,4,5,6,7,8,9,12,114,41];
