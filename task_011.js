69e8e8ec5df1fc74ef249b029e24a936436a07a1
yunuSdet <yunuskulcu84@gmail.com>
2023-11-28 15:56:02 +0300

    task_011.js is created

---
+++ task_011.js
@@ -0,0 +1,23 @@
+//Print the sum of numbers between 1 and 100 that are divisible by 5
+
+
+let sum = 0;
+
+
+//First solution
+for (let index = 1; index <= 100; index++) {
+       if(index % 5 == 0)  sum += index;
+}
+console.log(sum);
+
+
+//Second solution
+let numbers = Array.from(Array(100).keys());
+sum = numbers.map(i=>i+1).filter(i=>i % 5 == 0).reduce((a,b)=> a+b,0);
+console.log(sum);
+
+
+
+
+
+
