0a012e60b475da1488d60303b2fe4f3f59c48f29
yunuSdet <yunuskulcu84@gmail.com>
2023-11-29 00:21:35 +0300

    task_015.js is updated

--- task_015.js
+++ task_015.js
@@ -1,8 +1,9 @@
 //Find the number of positive divisors of a given number.
 
 let number = 1034;
-
+let divisors = [];
 for (let index = 1; index <= number; index++) {    
-    if(number % index == 0) console.log(index);
+    if(number % index == 0) divisors.push(index);
 }
+console.log(divisors);
 
